5869693acf5faf5a7044c6f689db0c05bbd131db
huycuuvan <huynd130904@gmail.com>
2025-09-17 10:35:22 +0700

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {MuiThemeProvider} from "./components/MuiThemeProvider"
-import 'react-resizable/css/styles.css'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
